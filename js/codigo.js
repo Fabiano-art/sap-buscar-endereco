f60f62bba26a8c3ffa8b6f250dc1ab74e2977fd3
Fabiano Guimarães <fabio.dub@outlook.com>
2021-04-29 23:50:04 -0300

    Finalizado versão 1

--- js/codigo.js
+++ js/codigo.js
@@ -8,12 +8,9 @@ function filtrar(json) {
     return enderecoFiltrado
 }
 
-const buttonPesquisar = document.querySelector('#pesquisar')
-buttonPesquisar.onclick = e => {
-
+function ajaxAndShow(e){
     const cep = document.querySelector('.campo-cep')
     const tr = document.querySelector('tr:nth-child(2n)')
-
     const url = `https://viacep.com.br/ws/${cep.value}/json/`
 
     fetch(url)
@@ -31,5 +28,51 @@ buttonPesquisar.onclick = e => {
             )
 
             tr.innerHTML = `<tr>${itens}</tr>`
+            adicionarHistorico(endereco)
         })
+}
+
+function ajaxFromHistorico(e){
+    const cep = e.previousElementSibling.innerText
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const tr = document.querySelector('tr:nth-child(2n)')
+
+    fetch(url)
+        .then(resp => resp.json())
+        .then(endereco => {
+            const dados = []
+
+            for (let property in endereco) {
+                dados.push(endereco[property])
+            }
+
+            filtrado = filtrar(dados)
+            const itens = filtrado.reduce(
+                (html, endereco) => html + `<td>${endereco}</td>`, ''
+            )
+            
+            tr.innerHTML = `<tr>${itens}</tr>`
+        })
+}
+
+function adicionarHistorico(endereco) {
+    const div = document.createElement('div')
+    div.classList.add('dados-historico')
+    const h2 = document.createElement('h2')
+    h2.id = 'cep-historico'
+    h2.innerHTML = endereco.cep
+    div.appendChild(h2)
+    div.insertAdjacentHTML("beforeend", "<button class='botao' onclick='ajaxFromHistorico(this)'>Visualizar</button>")
+
+    const historico = document.querySelector('.historico')
+    historico.appendChild(div)
+}
+
+const buttonPesquisar = document.querySelector('#pesquisar')
+buttonPesquisar.onclick = ajaxAndShow
+
+const buttonLimpar = document.querySelector('#limpar')
+buttonLimpar.onclick = e => {
+    const row = document.querySelector('tr:nth-child(2n)')
+    row.innerHTML = ''
 }
